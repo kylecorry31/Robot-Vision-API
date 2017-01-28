a4f74d8bd1bb889889e9a1b44f114bd309fd0f4a
Kyle Corry <kylecorry31@gmail.com>
2017-01-28 13:43:27 -0500

    Fix MicrosoftLifeCam view angle

--- src/com/kylecorry/frc/vision/CameraSpecs.java
+++ src/com/kylecorry/frc/vision/CameraSpecs.java
@@ -2,6 +2,6 @@ package com.kylecorry.frc.vision;
 
 public class CameraSpecs {
 	public static class MicrosoftLifeCam {
-		public static final double HORIZONTAL_VIEW_ANGLE = 56.5;
+		public static final double HORIZONTAL_VIEW_ANGLE = 25.233 * 2;
 	}
 }
