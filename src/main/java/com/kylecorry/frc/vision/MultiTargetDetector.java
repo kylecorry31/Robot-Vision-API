e7a72f9f892bc4da2fbdaea284e6e1fb51302ffa
Kyle Corry <kylecorry31@gmail.com>
2017-02-12 15:43:19 -0500

    Add JavaDocs to MultiTargetDetector

--- src/main/java/com/kylecorry/frc/vision/MultiTargetDetector.java
+++ src/main/java/com/kylecorry/frc/vision/MultiTargetDetector.java
@@ -6,44 +6,61 @@ import java.util.List;
 
 import org.opencv.core.Mat;
 
+// TODO: Add better way to identify which detector found what target
 public class MultiTargetDetector extends Detector<Target> {
 
-	private List<Detector<? extends Target>> detectors;
+    private List<Detector<? extends Target>> detectors;
 
-	public MultiTargetDetector(List<Detector<? extends Target>> detectors) {
-		this.detectors = detectors;
-	}
-
-	public MultiTargetDetector(Detector<? extends Target>... detectors){
-	    this(Arrays.asList(detectors));
+    /**
+     * Create a MultiTargetDetector to detect multiple types of targets in an image.
+     *
+     * @param detectors The detector for each target to identify.
+     */
+    public MultiTargetDetector(List<Detector<? extends Target>> detectors) {
+        this.detectors = detectors;
     }
 
-	@Override
-	public List<Target> detect(Mat frame) {
-		List<Target> targets = new ArrayList<>();
-		for (Detector<? extends Target> detector : detectors) {
-			targets.addAll(detector.detect(frame));
-		}
-		return targets;
-	}
+    /**
+     * Create a MultiTargetDetector to detect multiple types of targets in an image.
+     *
+     * @param detectors The detector for each target to identify.
+     */
+    public MultiTargetDetector(Detector<? extends Target>... detectors) {
+        this(Arrays.asList(detectors));
+    }
 
-	@Deprecated
-	public static class Builder {
+    /**
+     * Detect the targets in the image.
+     *
+     * @param frame The image to detect targets in.
+     * @return The list of possible targets ordered by confidence from greatest to least and by which detector found the target.
+     */
+    @Override
+    public List<Target> detect(Mat frame) {
+        List<Target> targets = new ArrayList<>();
+        for (Detector<? extends Target> detector : detectors) {
+            targets.addAll(detector.detect(frame));
+        }
+        return targets;
+    }
 
-		private List<Detector<? extends Target>> detectors;
+    @Deprecated
+    public static class Builder {
 
-		public Builder() {
-			detectors = new ArrayList<>();
-		}
+        private List<Detector<? extends Target>> detectors;
 
-		public Builder add(Detector<? extends Target> detector) {
-			detectors.add(detector);
-			return this;
-		}
+        public Builder() {
+            detectors = new ArrayList<>();
+        }
 
-		public MultiTargetDetector build() {
-			return new MultiTargetDetector(detectors);
-		}
-	}
+        public Builder add(Detector<? extends Target> detector) {
+            detectors.add(detector);
+            return this;
+        }
+
+        public MultiTargetDetector build() {
+            return new MultiTargetDetector(detectors);
+        }
+    }
 
 }
