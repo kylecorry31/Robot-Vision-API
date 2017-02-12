491e24f265772d5aeaf031252cb30ca24c03fe39
Kyle Corry <kylecorry31@gmail.com>
2017-02-12 15:24:45 -0500

    Add JavaDocs to CameraSpecs

--- src/main/java/com/kylecorry/frc/vision/CameraSpecs.java
+++ src/main/java/com/kylecorry/frc/vision/CameraSpecs.java
@@ -1,7 +1,10 @@
 package com.kylecorry.frc.vision;
 
 public class CameraSpecs {
-	public static class MicrosoftLifeCam {
-		public static final double HORIZONTAL_VIEW_ANGLE = 25.233 * 2;
-	}
+    public static class MicrosoftLifeCam {
+        /**
+         * The horizontal view angle of the Microsoft LifeCam in degrees.
+         */
+        public static final double HORIZONTAL_VIEW_ANGLE = 25.233 * 2;
+    }
 }
