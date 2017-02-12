77b9e6ac71e71c854f425c119b547dd89b3126b0
Kyle Corry <kylecorry31@gmail.com>
2017-02-12 09:20:46 -0500

    Add test file

--- src/test/java/Examples.java
+++ src/test/java/Examples.java
@@ -1,7 +1,51 @@
-package PACKAGE_NAME;
+import com.kylecorry.frc.vision.CameraSource;
+import com.kylecorry.frc.vision.TargetDetector;
+import com.kylecorry.frc.vision.TargetSpecs;
+import edu.wpi.cscore.UsbCamera;
+import org.junit.Test;
+import org.opencv.core.Range;
 
 /**
  * Created by Kylec on 2/12/2017.
  */
 public class Examples {
+
+    public void testCameraSource(){
+        CameraSource camera = new CameraSource(new UsbCamera("cam", 0), new TargetDetector(new TargetSpecs() {
+            @Override
+            public Range getHue() {
+                return null;
+            }
+
+            @Override
+            public Range getSaturation() {
+                return null;
+            }
+
+            @Override
+            public Range getValue() {
+                return null;
+            }
+
+            @Override
+            public double getWidth() {
+                return 0;
+            }
+
+            @Override
+            public double getHeight() {
+                return 0;
+            }
+
+            @Override
+            public double getArea() {
+                return 0;
+            }
+
+            @Override
+            public int getMinPixelArea() {
+                return 0;
+            }
+        }));
+    }
 }
