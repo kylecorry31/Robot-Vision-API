a964a795e12228d49723041201625b9e21cd9d54
Kyle Corry <kylecorry31@gmail.com>
2017-02-12 09:23:23 -0500

    Update test file to use CameraSource constructor without TargetDetector

--- src/test/java/Examples.java
+++ src/test/java/Examples.java
@@ -1,9 +1,6 @@
 import com.kylecorry.frc.vision.CameraSource;
-import com.kylecorry.frc.vision.TargetDetector;
-import com.kylecorry.frc.vision.TargetSpecs;
 import edu.wpi.cscore.UsbCamera;
 import org.junit.Test;
-import org.opencv.core.Range;
 
 /**
  * Created by Kylec on 2/12/2017.
@@ -11,41 +8,9 @@ import org.opencv.core.Range;
 public class Examples {
 
     public void testCameraSource(){
-        CameraSource camera = new CameraSource(new UsbCamera("cam", 0), new TargetDetector(new TargetSpecs() {
-            @Override
-            public Range getHue() {
-                return null;
-            }
-
-            @Override
-            public Range getSaturation() {
-                return null;
-            }
-
-            @Override
-            public Range getValue() {
-                return null;
-            }
-
-            @Override
-            public double getWidth() {
-                return 0;
-            }
-
-            @Override
-            public double getHeight() {
-                return 0;
-            }
-
-            @Override
-            public double getArea() {
-                return 0;
-            }
-
-            @Override
-            public int getMinPixelArea() {
-                return 0;
-            }
-        }));
+        CameraSource camera = new CameraSource(new UsbCamera("cam", 0));
+        camera.setExposure(0);
+        camera.setBrightness(0);
+        camera.start();
     }
 }
