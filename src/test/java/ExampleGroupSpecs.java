ed7c74f5db89f77bfd306eb997b789c32e3249b9
Kyle Corry <kylecorry31@gmail.com>
2017-03-08 19:59:23 -0500

    Add examples

---
+++ src/test/java/ExampleGroupSpecs.java
@@ -0,0 +1,31 @@
+import com.kylecorry.frc.vision.TargetGroupSpecs;
+
+/**
+ * Created by Kyle on 3/6/2017.
+ */
+public class ExampleGroupSpecs implements TargetGroupSpecs {
+    @Override
+    public double getTargetWidthRatio() {
+        return 0;
+    }
+
+    @Override
+    public double getTargetHeightRatio() {
+        return 0;
+    }
+
+    @Override
+    public Alignment getAlignment() {
+        return null;
+    }
+
+    @Override
+    public double getGroupWidth() {
+        return 0;
+    }
+
+    @Override
+    public double getGroupHeight() {
+        return 0;
+    }
+}
--- src/test/java/Examples.java
+++ src/test/java/Examples.java
@@ -1,7 +1,9 @@
-import com.kylecorry.frc.vision.CameraSource;
-import com.kylecorry.frc.vision.TargetDetector;
+import com.kylecorry.frc.vision.*;
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by Kylec on 2/12/2017.
@@ -9,20 +11,34 @@ import org.junit.Test;
 public class Examples {
 
     public void testCameraSource(){
-        CameraSource camera = new CameraSource(new UsbCamera("cam", 0));
-        camera.setExposure(0);
-        camera.setBrightness(0);
-        camera.setResolution(640, 480);
-        camera.start();
+        UsbCamera usbCamera = CameraServer.getInstance().startAutomaticCapture(0);
+        usbCamera.setExposureManual(0);
+        usbCamera.setBrightness(0);
+        usbCamera.setWhiteBalanceManual(10000);
+        usbCamera.setResolution(160, 120);
 
-        CameraSource camera2 = new CameraSource(new UsbCamera("cam", 1));
-        camera2.setExposure(0);
-        camera2.setBrightness(0);
-        camera2.setResolution(640, 480);
-        camera2.start();
+        CameraSource cameraSource = new CameraSource(usbCamera);
 
-        TargetDetector targetDetector = new TargetDetector(new ExampleSpecs());
-        camera.setDetector(targetDetector);
+        TargetGroupDetector detector = new TargetGroupDetector(new ExampleSpecs(), new ExampleGroupSpecs());
+
+        List<TargetGroup> targets = detector.detect(cameraSource.getPicture());
+
+        if(!targets.isEmpty()){
+            TargetGroup bestTarget = targets.get(0);
+
+            double angle = bestTarget.computeAngle(160, CameraSpecs.MicrosoftLifeCam.HORIZONTAL_VIEW_ANGLE);
+            double distance = bestTarget.computeDistance(160, new ExampleGroupSpecs().getGroupWidth(), CameraSpecs.MicrosoftLifeCam.HORIZONTAL_VIEW_ANGLE);
+
+            double centerX = bestTarget.getCenterPosition().x;
+
+            double confidence = bestTarget.getIsTargetGroupProbability(); // 0 - 1
+
+            if(confidence > 0.75){
+                // do something
+            }
+        }
 
     }
+
+
 }
