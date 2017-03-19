b406bafc7b18246416d9a1b849443ea437831fa9
Kyle Corry <kylecorry31@gmail.com>
2017-03-18 22:28:52 -0400

    Add method to calculate focal length from image width and view angle

--- src/main/java/com/kylecorry/frc/vision/CameraSpecs.java
+++ src/main/java/com/kylecorry/frc/vision/CameraSpecs.java
@@ -7,6 +7,20 @@ public class CameraSpecs {
          */
         public static final double HORIZONTAL_VIEW_ANGLE = 25.233 * 2;
 
+        /**
+         * The focal length of the Microsoft LifeCam in pixels.
+         */
         public static final double FOCAL_LENGTH_PIXELS = 1358.0384;
+    }
+
+    /**
+     * Calculate the focal length of the camera in pixels.
+     *
+     * @param fullImageWidth      The width of the full resolution image of the camera in pixels.
+     * @param horizontalViewAngle The horizontal view angle of the camera in degrees.
+     * @return The focal length of the camera in pixels.
+     */
+    public static double calculateFocalLengthPixels(int fullImageWidth, double horizontalViewAngle) {
+        return fullImageWidth / (2 * Math.tan(Math.toRadians(horizontalViewAngle / 2)));
     }
 }
