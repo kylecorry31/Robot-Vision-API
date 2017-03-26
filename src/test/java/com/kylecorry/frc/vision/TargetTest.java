91413f3afdb6053ade3ff37e819dceeabd07fafa
Kyle Corry <kylecorry31@gmail.com>
2017-03-26 13:37:57 -0400

    Added tests for angle calculations

---
+++ src/test/java/com/kylecorry/frc/vision/TargetTest.java
@@ -0,0 +1,20 @@
+package com.kylecorry.frc.vision;
+
+import com.kylecorry.geometry.Point;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kylec on 3/26/2017.
+ */
+public class TargetTest {
+
+    @Test
+    public void testAngle() {
+        Target target = new Target(1, 10, 10, new Point(40, 245, 0));
+        double angle = target.computeAngle(640, 60);
+        assertEquals(116.3, angle, 0.1);
+    }
+
+}
