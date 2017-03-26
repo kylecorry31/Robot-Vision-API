2041d23107699c1f99cf7671d4a290e91db209cc
Kyle Corry <kylecorry31@gmail.com>
2017-03-26 13:36:56 -0400

    Removed focalLengthPixel references in favor of view angle

--- src/main/java/com/kylecorry/frc/vision/Target.java
+++ src/main/java/com/kylecorry/frc/vision/Target.java
@@ -7,6 +7,14 @@ public class Target {
     private double width, height;
     private Point position;
 
+    /**
+     * Create a target.
+     *
+     * @param confidence The confidence that this is the real target from 0 to 1 inclusive.
+     * @param width      The width of the target in pixels.
+     * @param height     The height of the target in pixels.
+     * @param position   The top left position in pixels.
+     */
     Target(double confidence, double width, double height, Point position) {
         this.confidence = confidence;
         this.width = width;
@@ -64,11 +72,11 @@ public class Target {
      *
      * @param imageWidth             The width of the image.
      * @param heightRelativeToCamera The height of the target relative to the camera (distance from camera to target along Y axis).
-     * @param focalLengthPixels      The focal length of the camera in pixels.
+     * @param horizontalViewAngle      The horizontal view angle in degrees.
      * @return The distance to the target in the same units as the targetActualWidth.
      */
-    public double computeDistance(int imageWidth, double heightRelativeToCamera, double focalLengthPixels) {
-        return focalLengthPixels * heightRelativeToCamera / (getCenterPosition().y - imageWidth / 2.0 + 0.5);
+    public double computeDistance(int imageWidth, double heightRelativeToCamera, double horizontalViewAngle) {
+        return CameraSpecs.calculateFocalLengthPixels(imageWidth, horizontalViewAngle) * heightRelativeToCamera / (getCenterPosition().y - imageWidth / 2.0 + 0.5);
     }
 
     /**
@@ -77,11 +85,11 @@ public class Target {
      * To convert it to allow for the left of center to be negative, and right of center to be positive subtract this angle from 90.
      *
      * @param imageWidth      The width of the image in pixels.
-     * @param cameraViewAngle The view angle of the camera in degrees.
+     * @param horizontalViewAngle      The horizontal view angle in degrees.
      * @return The angle to the target from the coordinate frame centered on the camera.
      */
-    public double computeAngle(int imageWidth, double cameraViewAngle) {
-        return 90 - (getCenterPosition().x - imageWidth / 2.0 + 0.5) / (imageWidth / 2 * Math.tan(Math.toRadians(cameraViewAngle / 2.0)));
+    public double computeAngle(int imageWidth, double horizontalViewAngle) {
+        return 90 - Math.toDegrees(Math.atan((getCenterPosition().x - imageWidth / 2.0 + 0.5) / CameraSpecs.calculateFocalLengthPixels(imageWidth, horizontalViewAngle)));
     }
 
 }
