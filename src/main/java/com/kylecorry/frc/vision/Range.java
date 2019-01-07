6cfa6f6e66b381aed56c73a6c4046f60e1b9f0db
Kyle Corry <kylecorry31@gmail.com>
2019-01-07 15:23:56 -0500

    Add docs to misc classes

--- src/main/java/com/kylecorry/frc/vision/Range.java
+++ src/main/java/com/kylecorry/frc/vision/Range.java
@@ -1,27 +1,53 @@
 package com.kylecorry.frc.vision;
 
+/**
+ * A class which represents a numerical range.
+ */
 public class Range {
 
     private double low;
     private double high;
 
+    /**
+     * Creates a numerical range.
+     * @param low The minimum value in the range.
+     * @param high The maximum value in the range.
+     */
     public Range(double low, double high) {
         this.low = low;
         this.high = high;
     }
 
+    /**
+     * Gets the lowest value of the range.
+     * @return The lowest value.
+     */
     public double getLow() {
         return low;
     }
 
+    /**
+     * Gets the highest value of the range.
+     * @return The highest value.
+     */
     public double getHigh() {
         return high;
     }
 
+    /**
+     * Determines whether a value is in the range (inclusive).
+     * @param value The value to check for.
+     * @return True if the value is in the inclusive range.
+     */
     public boolean inRangeInclusive(double value){
         return getLow() <= value && getHigh() >= value;
     }
 
+    /**
+     * Determines whether a value is in the range (exclusive).
+     * @param value The value to check for.
+     * @return True if the value is in the exclusive range.
+     */
     public boolean inRangeExclusive(double value){
         return getLow() < value && getHigh() > value;
     }
--- src/main/java/com/kylecorry/frc/vision/Version.java
+++ src/main/java/com/kylecorry/frc/vision/Version.java
@@ -1,15 +1,30 @@
 package com.kylecorry.frc.vision;
 
+/**
+ * A class containing the current version numbers of the Robot Vision API.
+ */
 public class Version {
+    /**
+     * Get the major version.
+     * @return The major version.
+     */
     public int getMajorVersion(){
         return 1;
     }
 
+    /**
+     * Get the minor version.
+     * @return The minor version.
+     */
     public int getMinorVersion(){
         return 1;
     }
 
+    /**
+     * Get the patch version.
+     * @return The patch version.
+     */
     public int getPatchVersion(){
-        return 0;
+        return 1;
     }
 }
